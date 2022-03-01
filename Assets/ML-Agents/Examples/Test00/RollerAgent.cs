be3a10af81a7b483cdd7dede4cf9fc833e8a038f
DevBlackWolf <blackwolfdev1223@gmail.com>
2022-03-01 17:54:28 +0900

    2022/03/01 mlagent setting

--- Assets/ML-Agents/Examples/Test00/RollerAgent.cs
+++ Assets/ML-Agents/Examples/Test00/RollerAgent.cs
@@ -8,15 +8,18 @@ using Unity.MLAgents.Sensors;
 
 public class RollerAgent : Agent
 {
-    public Transform target;
+
     Rigidbody rBody;
 
+    public Transform target;
+    public float forceMultiplier = 10;
 
     private void Start()
     {
         rBody = GetComponent<Rigidbody>();
     }
 
+    //When the new episode starts, the agent's position is reset again.
     public override void OnEpisodeBegin()
     {
         //When the agent falls below the floor
@@ -29,4 +32,49 @@ public class RollerAgent : Agent
         //At the beginning of the episode, the target position is changed to random
         target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
     }
+
+    //Delivering observation information to the Machine learning program
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        //Deliver Target and Agent position
+        sensor.AddObservation(target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+
+        //Deliver the current agent's velocity
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+
+    }
+
+    //
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        //Move agent for learning.
+        //Actions size=2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionBuffers.ContinuousActions[0];
+        controlSignal.z = actionBuffers.ContinuousActions[1];
+        rBody.AddForce(controlSignal * forceMultiplier);
+
+        //Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, target.localPosition);
+
+        if (distanceToTarget < 1.42f) //When finding a target, reward it and end the episode.
+        {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+        else if (this.transform.localPosition.y < 0) //If it falls below the floor, it ends learning.
+        {
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continousActionsOut = actionsOut.ContinuousActions;
+        continousActionsOut[0] = Input.GetAxis("Horizontal");
+        continousActionsOut[1] = Input.GetAxis("Vertical");
+    }
+
 }
